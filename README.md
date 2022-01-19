054dc4ed02c92e15741be5a5ae3e257a3faa8f2b
sivamparama <sivamparama231@gmail.com>
2022-01-19 18:51:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-uobhn3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-uobhn3)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, VERSION } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+  
+  onSubmit(form:NgForm){
+    console.log(form);
+  }
+}
